9276c9521e96f6450113f8d89d6c102691e1cf51
Sankha Bhattacharjee <sankhab1999@gmail.com>
2022-07-14 13:05:48 +0530

    Add new todo functionality

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="home pa-5">
+    <v-btn class="ma-2" outlined color="primary" @click="toggleAddTaskForm">
+      {{ showAddFormButtonText }}
+    </v-btn>
+    <div v-if="showAddForm">
+      <v-text-field
+        v-model="newTaskTitle"
+        @blur="checkInput"
+        label="Add Title *"
+        clearable
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="newTaskSubTitle"
+        label="Add Description"
+        clearable
+      ></v-text-field>
+
+      <v-btn class="mr-4" type="submit" :disabled="invalid" color="primary" @click="addTask">
+        submit
+      </v-btn>
+    </div>
     <v-list flat three-line>
       <div v-for="task in tasks" :key="task.id">
         <v-list-item
@@ -18,8 +39,8 @@
               <v-list-item-subtitle>{{ task.subTitle }}</v-list-item-subtitle>
             </v-list-item-content>
             <v-list-item-action>
-              <v-btn icon>
-                <v-icon color="grey lighten-1">mdi-delete</v-icon>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
               </v-btn>
             </v-list-item-action>
           </template>
@@ -34,6 +55,10 @@
 export default {
   data() {
     return {
+      showAddForm: false,
+      newTaskTitle:'',
+      newTaskSubTitle:'',
+      invalid: true,
       tasks: [
         {
           id: 1,
@@ -56,11 +81,40 @@ export default {
       ],
     };
   },
+  computed: {
+    showAddFormButtonText() {
+      return this.showAddForm ? "Close" : "Add Todo";
+    },
+  },
   methods: {
+    addTask(){
+      //console.log('addTask');
+      const newTask ={
+        id: Date.now().toString(),
+        title: this.newTaskTitle,
+        subTitle: this.newTaskSubTitle,
+        done: false
+      };
+      this.tasks.push(newTask);
+      this.newTaskTitle = '';
+      this.newTaskSubTitle = '';
+
+    },
+    checkInput(){
+      if(this.newTaskTitle){
+        this.invalid = false;
+      }
+    },
     completeTask(id) {
-      console.log("id: ", id);
+      //console.log("id: ", id);
       const task = this.tasks.find((t) => t.id === id);
       task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((t) => t.id !== id);
+    },
+    toggleAddTaskForm() {
+      this.showAddForm = !this.showAddForm;
     },
   },
 };
