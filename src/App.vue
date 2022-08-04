8e1ba269067d3873d8b03ce5efe258f8daf06001
Bhattacharjee <BHATTS38@KITS.KFPLC.COM>
2022-08-04 23:58:26 +0530

    Add profile and search bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>UserName</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6"> Todo App </v-list-item-title>
@@ -23,12 +35,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      app
-      color="primary"
-      dark
-      prominent
-    >
+    <v-app-bar app color="primary" dark prominent>
       <template v-slot:img="{ props }">
         <v-img
           v-bind="props"
@@ -36,16 +43,16 @@
         ></v-img>
       </template>
 
-      <v-app-bar-nav-icon @click="drawer = !drawer "></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-app-bar-title>Todo List</v-app-bar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
+      <search-key v-if="isSearchTextBox" />
+      <v-btn icon v-else>
+        <v-icon @click="toggleSearchTextBox">mdi-magnify</v-icon>
       </v-btn>
-
     </v-app-bar>
 
     <v-main>
@@ -55,13 +62,24 @@
 </template>
 
 <script>
+import SearchKey from "./components/SearchTask.vue";
 export default {
+  components: {
+    SearchKey,
+  },
   data: () => ({
     drawer: null,
+    isSearchTextBox: false,
     items: [
-      { title: "Todo", icon: "mdi-format-list-checks", to: '/' },
-      { title: "About", icon: "mdi-information", to: '/about' },
+      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+      { title: "About", icon: "mdi-information", to: "/about" },
     ],
   }),
+  methods: {
+    toggleSearchTextBox() {
+      this.isSearchTextBox = !this.isSearchTextBox;
+      console.log(this.isSearchTextBox);
+    },
+  },
 };
 </script>
